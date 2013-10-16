387e93cf5b5f9110cfa293d832c93ae0a0ce4d56
lichengwu <ol_l@msn.cn>
2013-10-16 09:43:54 +0800

    test

---
+++ study/src/study.cpp
@@ -0,0 +1,19 @@
+/*
+ * study.cpp
+ *
+ *  Created on: Oct 16, 2013
+ *      Author: zuojing
+ */
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+	cout << "test project" << endl;
+
+	return 0;
+}
+
